17498a54c660fd7314881ccd9772dbbc6fd53780
ArkadiyN <Arkadiy.naumov@gmail.com>
2014-11-28 02:07:17 -0500

    Using reassignments
    Using an example from SoDA to create a snippet of code setting a matrix and its inverse via reassignment functions. Using matrix comparison from Rui Barradas (https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html)

--- cachematrix.R
+++ cachematrix.R
@@ -1,68 +1,41 @@
+# usage:
+        #create a matrix and assign it to object x
+        #myData <- cacheMtrxInit()
+        #setMtrx(myData)<-x
+        #getInv(myData)
 
-## The set of two functions computes or obtains from memory (if already available)
-## an inverse of a matrix that was passed as input into makeCacheMatrix function. 
-## The output of makeCacheMatrix serves as input into cacheSolve function.
 
+#initializes a list that contains objects for matrix and its inverse
+cacheMtrxInit <-function ()
+        list(obj=matrix(0),param = matrix(0))
 
-## makeCacheMatrix function clears out a local variable m and creates a list of 4 functions.
-## Each function is a named element in the list,
-## and can be called via a reference to a list element (list_name$function_name)
-
-makeCacheMatrix <- function(x) {
-        m <- NULL
-        ## local object y is assigned to object x (x is inside makeCacheMatrix function)
-        ## object m (inside makeCacheMatrix function) is set to NULL
-        
-        setObj <- function(y) {
-                        x <<- y
-                        m <<- NULL
+#reassignment function - compares the existing object from the Mtrx list with one that's passed as "value"
+#if the two are different - updates object for matrix and its inverse in Mtrx list by calling "resetMtrx" on Mtrx with value
+'setMtrx<-' <- function(Mtrx, value) {
+        if (matequal(Mtrx$obj,value) == FALSE) {
+                print("matrix updated")
+                resetMtrx(Mtrx, value)
                 }
-        
-        ## Returns object x. Since x is a free variable, the function will return
-        ## object x from parent (makeCacheMatrix function) environemnt (if one exists) 
-        ## or if not then from global environment
-        
-        getObj <- function() x
-        
-        ## Assigns its input to object m in parent environemnt (makeCacheMatrix function)
-        
-        setParam <- function(Param) m <<- Param
-        
-        ##Returns object m. Since m is a free variable, the function will return
-        ## object m from parent (makeCacheMatrix function) environemnt (if one exists) 
-        ## or if not then from global environment
-        
-        getParam <- function() m
-        
-        ## assigns 4 functions defined above to a list where names of the elements 
-        ## match function names (helps readability)
-        
-        list(setObj = setObj, 
-                getObj = getObj,
-                setParam = setParam,
-                getParam = getParam)
+        else {
+        print("same matrix alread exists")
+        Mtrx
+        }
 }
 
+#assigns new matrix value, computes its inverse and updates Mtrx list        
+resetMtrx <-function (Mtrx, value) {
+        Mtrx$obj <- value
+        Mtrx$param <- solve(value)
+        Mtrx
+}
 
-## cachSolve takes a list of functions (an output from makeCacheMatrix) as an input
-## and checks whether an object m exists within makeCacheMatrix.
-## if m exists within makeCacheMatrix (or global env), m returned as the result
-## Otherwise, the function:
-        ##obtains object x from makeCacheMatrix (or global env),
-        ## computes inverse of x
-        ## assign the result to m in makeCacheMatrix env
-        ## returns inverse of x as the result
-
-
-cacheSolve <- function(xList) {
+#grabs the inverse from the list if the inverse exists
+getInv <- function (Mtrx) {
+        if (!matequal(Mtrx$param,matrix(0))) Mtrx$param
+        else print("matrix is not initialized")
                 
-        m <- xList$getParam()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- xList$getObj()
-        m <- solve(data)
-        xList$setParam(m)
-        m
-}        
+}
+
+#matrices comparison         
+matequal <- function(x, y)
+        is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
